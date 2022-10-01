2bf91a24d33c000009d9355eb2b9a36e23a5fa2c
Madhan Paidimarla <pmmreddynitw@gmail.com>
2022-09-30 17:54:39 -0700

    added logo

--- src/AddReservation.js
+++ src/AddReservation.js
@@ -1,9 +1,41 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { faker } from "@faker-js/faker";
 
 export const AddReservation = () => {
   const [maxDate, setMaxdate] = useState(new Date());
   const [status, setStatus] = useState("");
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    await axios
+      .get("https://reserve-bbq-area-api.herokuapp.com/getreservations")
+      .then(async (res) => {
+        const data = res.data;
+        const futureReservations = await data.filter((f) =>
+          filterPastReservations(f)
+        );
+        setUsers(futureReservations);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const filterPastReservations = (f) => {
+    const today = new Date();
+    const date = new Date(f.date);
+    if (today.getTime() <= date.getTime()) {
+      return true;
+    } else {
+      axios
+        .delete(`https://reserve-bbq-area-api.herokuapp.com/delete/${f._id}`)
+        .then((res) => {
+          console.log("deleted", f);
+        });
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   const max =
     maxDate.getFullYear() +
@@ -28,6 +60,7 @@ export const AddReservation = () => {
         reservation
       )
       .then((res) => {
+        setUsers([...users, reservation]);
         setStatus("Succesfully reserved the BBQ area");
       })
       .catch((err) => {
@@ -117,6 +150,25 @@ export const AddReservation = () => {
         </form>
         <div>{status}</div>
       </article>
+      <h3>Reservations</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, name, date, unit, timeFrom, timeTo } = user;
+          return (
+            <li key={id}>
+              <img src={faker.internet.avatar()} />
+              <div>
+                <h4>
+                  {name}, Unit : {unit}{" "}
+                </h4>
+                <h4>{date}</h4>
+                {timeFrom} to {timeTo}
+              </div>
+              <div></div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <br/>
     <br/>
     <AddReservation/>
-    <ShowReservations/>
   </div>;
 }
 
