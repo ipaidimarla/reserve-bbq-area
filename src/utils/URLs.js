3880facba38d248d53b02588538d721aa6b0edbf
Madhan Paidimarla <pmmreddynitw@gmail.com>
2022-12-08 14:21:19 -0800

    added user url

--- src/utils/URLs.js
+++ src/utils/URLs.js
@@ -4,4 +4,4 @@ export const DEV_USER_URL = "http://localhost:3001/api/user";
 export const PROD_RES_URL =
   "https://reserve-bbq-api-g2jw-main-nczvuuileq-wm.a.run.app/api/reservations/";
 export const PROD_USER_URL =
-  "https://villages-bbq-reservations.herokuapp.com/api/user/";
+  "https://reserve-bbq-api-g2jw-main-nczvuuileq-wm.a.run.app/api/user/";
